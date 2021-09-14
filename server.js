76484bfc53fa950a014838a2843c7fbeb80bf933
Rishi-121 <dashrushikesh1121@gmail.com>
2021-09-15 04:31:32 +0530

    added the socket part

--- server.js
+++ server.js
@@ -3,6 +3,34 @@ const { PORT } = require("./config/env");
 const app = require("./app");
 
 const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+var users = [];
+
+io.on("connection", (socket) => {
+
+    socket.on("loggedin_user", (userId) => {
+
+        let userObj = { userId, socketId: socket.id };
+
+        users.push(userObj);
+    });
+
+    socket.on("send_notifications", (userId) => {
+
+        let user = users.find((e) => e.userId === userId);
+
+        let msg = "A test notification send by admin";
+
+        if (user && user !== undefined) {
+            io.to(user.socketId).emit("notification_received_by_admin", msg);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        users = users.filter((e) => e.socketId !== socket.id);
+    });
+});
 
 server.listen(
     PORT,
